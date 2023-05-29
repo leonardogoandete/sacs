6651d5e363c19b109ee324dcd3c3e0e2e7a50edc
leonardogoandete <leonardogoandete>
2023-05-29 18:16:28 -0300

    Ajustado App

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 import Home from './components/Home/Home';
 import Chamado from './components/TelaAbrirChamado/Chamado/Chamado';
+import ListarChamados from './components/TelaListarChamados/ListarChamados'
 
 export default function App() {
   return (
     <>
-      <Chamado/>
+      <ListarChamados/>
     </>
   );
 }
