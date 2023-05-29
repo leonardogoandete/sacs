edab2254c76274fba012a85f49d621279cf16be2
leonardo_goandete <leonardo_goandete>
2023-05-28 22:40:27 -0300

    ajustes

--- src/components/Card/CardChamado.jsx
+++ src/components/Card/CardChamado.jsx
@@ -25,14 +25,13 @@ export function CardChamado({ imageSrc, title, content }) {
       overflow="hidden"
       width={["100%", "100%", "564px", "564px"]}
       height={["100%", "100%", "170px", "170px"]}
-      marginBottom={4}
       cursor="pointer"
       onClick={handleOpenModal}
       bg={isDark ? "gray.800" : "#F2FFEE"}
       marginLeft={isMobile ? 0 : "58px"}
       p={isMobile ? 2 : 0}
     >
-      <Flex flexDirection={isMobile ? "column" : ["column", "column", "row", "row"]} justifyContent={isMobile ? "center" : "flex-start"} alignItems={isMobile ? "center" : "flex-start"} >
+      <Flex flexDirection={isMobile ? "column" : ["column", "column", "row", "row"]} justifyContent={isMobile ? "center" : "flex-start"} alignItems={isMobile ? "center" : "flex-start"}>
         <Image src={imageSrc} alt="Imagem" width={imageWidth} height={imageHeight} ml={isMobile ? 2 : 50} mt={isMobile ? 4 : 8} />
         <Box p={4} marginLeft={isMobile ? 0 : "30px"} mt={isMobile ? 2 : 4}>
           <Heading as="h3" fontSize={isMobile ? "16px" : "xl"} mb={2}>
--- src/components/Chamado/Chamado.jsx
+++ src/components/Chamado/Chamado.jsx
@@ -1,7 +1,8 @@
-import { Box, Flex, SimpleGrid } from "@chakra-ui/react";
+import { Box, Flex, SimpleGrid, Heading, useBreakpointValue } from "@chakra-ui/react";
 import { CardChamado } from "../Card/CardChamado";
 
 export function Chamado() {
+    const isMobile = useBreakpointValue({ base: true, lg: false });
     const cardsData = [
         {
             imageSrc: "img/projetor.png",
@@ -37,19 +38,24 @@ export function Chamado() {
     ];
 
     return (
-        <Flex justifyContent="center">
-            <Box p={4}>
-                <SimpleGrid columns={[1, 1, 2, 2]} spacing={2} p={4}> {/* 1 coluna em telas pequenas, 2 colunas em telas maiores */}
-                    {cardsData.map((card, index) => (
-                        <CardChamado
-                        key={index}
-                        imageSrc={card.imageSrc}
-                        title={card.title}
-                        content={card.content}
-                    />
-                    ))}
-                </SimpleGrid>
-            </Box>
-        </Flex>
+        <>
+            <Flex justifyContent="center">
+                <Box p={4}>
+                    <Heading as="h3" fontSize={isMobile ? "20px" : "xl"} mb={4} textAlign="center" mt={6}>
+                        Abertura de Chamado
+                    </Heading>
+                    <SimpleGrid columns={[1, 1, 2, 2]} spacing={2} p={4}> {/* 1 coluna em telas pequenas, 2 colunas em telas maiores */}
+                        {cardsData.map((card, index) => (
+                            <CardChamado
+                                key={index}
+                                imageSrc={card.imageSrc}
+                                title={card.title}
+                                content={card.content}
+                            />
+                        ))}
+                    </SimpleGrid>
+                </Box>
+            </Flex>
+        </>
     );
 }
