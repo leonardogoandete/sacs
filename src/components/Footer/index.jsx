d81917dabcc6983e8aaf71fda20aa535b443529f
leonardogoandete <leonardogoandete>
2023-06-13 20:11:05 -0300

    Ajustado imagem footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -26,13 +26,13 @@ export const Footer = () => {
   return (
     <FooterStyle>
       <Box p={4} height={isMobile ? "auto" : "100px"} zIndex={1} mb={4}>
-        <Flex flexDirection={isMobile ? "column" : "row"} alignItems={isMobile ? "center" : "flex-start"} justifyContent="center">
-          <Text textAlign="center" fontSize={isMobile ? "10px" : "sm"} color="white" mb={isMobile ? 2 : 0}>
+        <Flex flexDirection={isMobile ? "column" : "row"} alignItems="center" justifyContent="center">
+          <Image src="img/ifrs-poa.png" alt="Logo IFRS POA" mt={0} maxW={isMobile? "50%" : "35%"}/>
+        </Flex>
+        <Flex flexDirection={isMobile ? "column" : "row"} alignItems={isMobile ? "center" : "flex-start"} justifyContent="center" mt={2}>
+          <Text textAlign="center" fontSize={isMobile ? "10px" : "sm"} color="white">
             &copy; {new Date().getFullYear()} SACS - Sistema de Atendimento a Chamados e Suporte. Todos os direitos reservados.
           </Text>
-        </Flex>
-        <Flex flexDirection={isMobile ? "column" : "row"} alignItems="center" justifyContent="center">
-          <Image src="img/ifrs-poa.png" alt="Logo IFRS POA" mt={2} maxW="100%" />
         </Flex>
       </Box>
     </FooterStyle>
